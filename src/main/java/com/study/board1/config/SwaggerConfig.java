769c63cd146372451f13860f6e78c8af431d308a
jisoleil <janggh1012@naver.com>
2023-07-06 21:15:09 +0900

    swagger 연동

---
+++ src/main/java/com/study/board1/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.study.board1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo()); // apiInfo() 메서드 호출
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("First Spring Boot Board REST API")
+                .version("1.0.0")
+                .description("첫번째 스프링 부트 프로젝트인 게시판 swagger api 입니다.")
+                .build();
+    }
+}
--- src/main/java/com/study/board1/controller/BoardController.java
+++ src/main/java/com/study/board1/controller/BoardController.java
@@ -7,14 +7,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
@@ -95,3 +92,5 @@ public class BoardController {
         return "message";
     }
 }
+
+
